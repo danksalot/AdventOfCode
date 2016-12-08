481c15754994d4d43a61b1f1711546b9092c6f26
danksalot <danksalot@users.noreply.github.com>
2016-12-08 01:31:04 -0600

    Update FoundOnline.py

--- 2016/Day7/FoundOnline.py
+++ 2016/Day7/FoundOnline.py
@@ -1,15 +1,9 @@
 #Solution by user Barnybug found here: https://www.reddit.com/r/adventofcode/comments/5gy1f2/2016_day_7_solutions/
 
 import re
-
 def abba(x):
     return any(a == d and b == c and a != b for a, b, c, d in zip(x, x[1:], x[2:], x[3:]))
-
-def aba(sn, hn):
-    return any(a == c and a != b and b+a+b in hn for a, b, c in zip(sn, sn[1:], sn[2:]))
-
-lines = [re.split(r'\[([^\]]+)\]', line) for line in open('Input.txt')]
+lines = [re.split(r'\[([^\]]+)\]', line) for line in open('input.txt')]
 parts = [(' '.join(p[::2]), ' '.join(p[1::2])) for p in lines]
-
-print 'Answer #1:', sum(abba(sn) and not (abba(hn)) for sn, hn in parts)
-print 'Answer #2:', sum(aba(sn, hn) for sn, hn in parts)
+print('Answer #1:', sum(abba(sn) and not(abba(hn)) for sn, hn in parts))
+print('Answer #2:', sum(any(a == c and a != b and b+a+b in hn for a, b, c in zip(sn, sn[1:], sn[2:])) for sn, hn in parts))
