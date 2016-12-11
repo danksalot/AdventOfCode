eb7fcca52e6fae68e9ea7bf9fff3545d6b3d0f64
danksalot <danksalot@users.noreply.github.com>
2016-12-11 05:13:11 -0600

    Update Program.py

--- 2016/Day11/Program.py
+++ 2016/Day11/Program.py
@@ -1,1 +1,2 @@
-print sum(2 * sum([8,2,0,0][:x]) - 3 for x in range(1,4))
+print "Part1:", sum(2 * sum([8,2,0,0][:x]) - 3 for x in range(1,4))
+print "Part2:", sum(2 * sum([12,2,0,0][:x]) - 3 for x in range(1,4))
