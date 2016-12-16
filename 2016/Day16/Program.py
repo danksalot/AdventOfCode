3b8b0701b402ca86fe2ac20bac1c273ba7b52aa6
Jacob Danks <danksalot2000@yahoo.com>
2016-12-16 00:32:57 -0600

    refactoring Day16

--- 2016/Day16/Program.py
+++ 2016/Day16/Program.py
@@ -1,12 +1,12 @@
-data = "11101000110010100"
-diskLength = 272	# Part2 - 35651584
+def expandData(data, length):
+	while len(data) < length:
+		data += "0" + ''.join("0" if n == "1" else "1" for n in data[::-1])
+	return data[:length]
 
-while len(data) < diskLength:
-	data += "0" + ''.join("0" if n == "1" else "1" for n in data[::-1])
+def contractChecksum(checksum):
+	while not len(checksum) & 1:
+		checksum = ''.join("1" if x == y else "0" for x, y in zip(checksum[0::2], checksum[1::2]))
+	return checksum
 
-checksum = data[:diskLength]
-
-while len(checksum) % 2 == 0:
-	checksum = ''.join("1" if x == y else "0" for x, y in zip(checksum[0::2], checksum[1::2]))
-
-print checksum
+print contractChecksum(expandData("11101000110010100", 272))
+print contractChecksum(expandData("11101000110010100", 35651584))
