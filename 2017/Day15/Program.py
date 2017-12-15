9bb3f08a01ad44e8be53bb7ee77728152529507d
Jacob Danks <danksalot2000@yahoo.com>
2017-12-15 01:24:58 -0600

    refactoring

--- 2017/Day15/Program.py
+++ 2017/Day15/Program.py
@@ -1,47 +1,19 @@
-valueA = 699
-valueB = 124
-
-def genA(current, mod):
+def makeGenerator(current, factor, mod):
 	for i in range(40000000):
-		current = (current * 16807) % 2147483647
+		current = (current * factor) % 2147483647
 		if current % mod == 0:
 			yield current
 
-def genB(current, mod):
-	for i in range(40000000):
-		current = (current * 48271) % 2147483647
-		if current % mod == 0:
-			yield current
+def matchGenerators(left, right):
+	matches = 0
+	while True:	
+		valueA = next(left, None)
+		valueB = next(right, None)
+		if valueA and valueB:
+			matches += not ((valueA & 65535) ^ (valueB & 65535))
+		else:
+			break
+	return matches
 
-def compare(left, right):
-	return not ((left & 65535) ^ (right & 65535))
-
-GEN_A = genA(699, 1)
-GEN_B = genB(124, 1)
-
-matches = 0
-
-while True:	
-	valueA = next(GEN_A, None)
-	valueB = next(GEN_B, None)
-	if valueA and valueB:
-		matches += not ((valueA & 65535) ^ (valueB & 65535))
-	else:
-		break
-
-print 'Part1:', matches
-
-matches = 0
-GEN_A = genA(699, 4)
-GEN_B = genB(124, 8)
-
-while True:
-	valueA = next(GEN_A, None)
-	valueB = next(GEN_B, None)
-	if valueA and valueB:
-		matches += not ((valueA & 65535) ^ (valueB & 65535))
-	else:
-		break
-	
-print 'Part2:', matches
-
+print 'Part1:', matchGenerators(makeGenerator(699, 16807, 1), makeGenerator(124, 48271, 1))
+print 'Part2:', matchGenerators(makeGenerator(699, 16807, 4), makeGenerator(124, 48271, 8))
