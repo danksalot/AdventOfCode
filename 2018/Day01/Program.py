c6e45e4a4b4739893b63df0f5ed66d4afb1ad4b4
Jacob Danks <danksalot@gmail.com>
2018-11-30 23:40:51 -0600

    optimized a bit more

--- 2018/Day01/Program.py
+++ 2018/Day01/Program.py
@@ -1,17 +1,14 @@
 total = 0
 values = []
-firstTime = True
 
-while True:
-	with open('Input') as inFile:
-		for line in inFile.read().splitlines():
+with open('Input') as inFile:
+	print('Part 1', sum(int(line) for line in inFile))
+
+	while True:
+		for line in inFile:
 			total += int(line)
 			if (total in values):
 				print('Part 2:', total)
 				exit();
 			else:
 				values.append(total)
-
-	if (firstTime):
-		print('Part 1:', total)
-		firstTime = False
