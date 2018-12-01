6ce964719fd8fc201b8e9f2bc5c11049db498071
Jacob Danks <danksalot@gmail.com>
2018-11-30 23:31:07 -0600

    finished 2018 day 1

--- 2018/Day01/Program.py
+++ 2018/Day01/Program.py
@@ -1,7 +1,18 @@
-with open('Input') as inFile:
-	for line in inFile:
-		print(line)
+total = 0
+values = []
+firstTime = True
 
-with open('Input') as inFile:
-	inputString = inFile.read()
-	print(line)
+while True:
+	with open('Input') as inFile:
+		for line in inFile.read().splitlines():
+			num = line.replace('+', '')
+			total += int(num)
+			if (total in values):
+				print('Part 2:', total)
+				exit();
+			else:
+				values.append(total)
+
+	if (firstTime):
+		print('Part 1:', total)
+		firstTime = False
