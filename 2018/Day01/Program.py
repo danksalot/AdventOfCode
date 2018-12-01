d2d3b45909db6c0926ec6d691a4d10ca8c348ef3
Jacob Danks <danksalot@gmail.com>
2018-12-01 00:08:20 -0600

    store a copy of the lines

--- 2018/Day01/Program.py
+++ 2018/Day01/Program.py
@@ -2,10 +2,11 @@ total = 0
 values = []
 
 with open('Input') as inFile:
-	print('Part 1', sum(int(line) for line in inFile))
+	lines = inFile.readlines()
+	print('Part 1:', sum(int(line) for line in lines))
 
 	while True:
-		for line in inFile:
+		for line in lines:
 			total += int(line)
 			if (total in values):
 				print('Part 2:', total)
