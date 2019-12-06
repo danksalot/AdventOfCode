0d14e9966c44bf66ca168479c9133a5d51a2db55
Jacob Danks <danksalot@gmail.com>
2019-12-06 00:49:27 -0600

    Update using NetworkX

--- 2019/Day06/Program.py
+++ 2019/Day06/Program.py
@@ -1,12 +1,11 @@
-objects = {}
-com = None
-numOrbits = 0
 
 with open('Input') as inFile:
 	lines = inFile.read().split('\n')
 	objects = [line.split(")") for line in lines]
 	orbits = { orbiter:{ 'parent': orbitee} for orbitee, orbiter in objects }
 
+# Part 1
+numOrbits = 0
 for currentObject in orbits.keys():
 	while currentObject != 'COM':
 		currentObject = orbits[currentObject]['parent']
@@ -14,6 +13,7 @@ for currentObject in orbits.keys():
 
 print("Part 1:", numOrbits)
 
+# Part 2
 myPath = ['YOU']
 while 'COM' not in myPath:
 	nextParent = orbits[myPath[-1]]['parent']
