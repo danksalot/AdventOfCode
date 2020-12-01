73de9710aa743fd37db2b1e9c8deb2241a431079
Jacob Danks <jacob.danks@inflection.com>
2020-12-01 14:55:25 -0600

    More formatting

--- 2020/Day01/Program.py
+++ 2020/Day01/Program.py
@@ -4,8 +4,8 @@ from math import prod
 with open('Input') as inFile:
 	lines = [int(x) for x in inFile.readlines()]
 
-	for x in combinations(lines, 2):
-		if sum(x) == 2020: print('Part 1: ' + str(prod(x)))
+	for c in combinations(lines, 2):
+		if sum(c) == 2020: print('Part 1:', prod(c))
 
-	for x in combinations(lines, 3):
-		if sum(x) == 2020: print('Part 2: ' + str(prod(x)))
+	for c in combinations(lines, 3):
+		if sum(c) == 2020: print('Part 2:', prod(c))
