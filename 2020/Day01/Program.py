2103ab4a76f35f503646f88a1bf9e8b0c64506f6
Jacob Danks <jacob.danks@inflection.com>
2020-12-01 10:55:37 -0600

    formatting

--- 2020/Day01/Program.py
+++ 2020/Day01/Program.py
@@ -5,11 +5,9 @@ with open('Input') as inFile:
 	lines = [int(x) for x in inFile.readlines()]
 
 	for x in combinations(lines, 2):
-		if sum(x) == 2020:
-			print('Part 1: ' + str(prod(x)))
+		if sum(x) == 2020: print('Part 1: ' + str(prod(x)))
 
 	for x in combinations(lines, 3):
-		if sum(x) == 2020:
-			print('Part 2: ' + str(prod(x)))
+		if sum(x) == 2020: print('Part 2: ' + str(prod(x)))
 
 				
