29c78d1f1c44c8e42ce305c9ee69a4793f63da55
Jacob Danks <jacob.danks@inflection.com>
2020-12-01 10:37:03 -0600

    Use some libraries to clean up

--- 2020/Day01/Program.py
+++ 2020/Day01/Program.py
@@ -1,14 +1,15 @@
+from itertools import combinations
+from numpy import prod
+
 with open('Input') as inFile:
 	lines = [int(x) for x in inFile.readlines()]
 
-	for i in range(len(lines)-1):
-		for j in range(1, len(lines)):
-			if ((lines[i] + lines[j]) == 2020):
-				print('Part 1: ' + str(lines[i] * lines[j]))
+	for x in combinations(lines, 2):
+		if sum(x) == 2020:
+			print('Part 1: ' + str(prod(x)))
 
-	for i in range(len(lines) - 2):
-		for j in range(1, len(lines) - 1):
-			for k in range(2, len(lines)):
-				if ((lines[i] + lines[j] + lines[k]) == 2020):
-					print('Part 2: ' + str(lines[i] * lines[j] * lines[k]))
+	for x in combinations(lines, 3):
+		if sum(x) == 2020:
+			print('Part 2: ' + str(prod(x)))
+
 				
