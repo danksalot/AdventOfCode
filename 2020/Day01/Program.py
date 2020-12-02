72c6f5e7932c6b73f2124c2d8a3d7acde8a6ab72
Jacob Danks <jacob.danks@inflection.com>
2020-12-01 23:34:52 -0600

    2020 Day2

--- 2020/Day01/Program.py
+++ 2020/Day01/Program.py
@@ -4,8 +4,8 @@ from math import prod
 with open('Input') as inFile:
 	lines = [int(x) for x in inFile.readlines()]
 
-	for c in combinations(lines, 2):
-		if sum(c) == 2020: print('Part 1:', prod(c))
+for c in combinations(lines, 2):
+	if sum(c) == 2020: print('Part 1:', prod(c))
 
-	for c in combinations(lines, 3):
-		if sum(c) == 2020: print('Part 2:', prod(c))
+for c in combinations(lines, 3):
+	if sum(c) == 2020: print('Part 2:', prod(c))
--- 2020/Day02/Program.py
+++ 2020/Day02/Program.py
@@ -1,5 +1,23 @@
 with open('Input') as inFile:
-	lines = [int(x) for x in inFile.readlines()]
+	lines = inFile.read().splitlines()
 
-	for line in lines:
-		print(line)
+validCount1 = 0
+validCount2 = 0
+
+for line in lines:
+	parts = line.split(' ')
+	bounds = parts[0].split('-')
+	lower = int(bounds[0])
+	upper = int(bounds[1])
+	letter = parts[1].rstrip(':')
+	password = parts[2]
+
+	count = password.count(letter)
+	if count >= lower and count <= upper:
+		validCount1 = validCount1 + 1
+
+	if (password[lower - 1] == letter or password[upper - 1] == letter) and password[lower - 1] != password[upper - 1]:
+		validCount2 = validCount2 + 1
+
+print('Part 1:', validCount1)
+print('Part 2:', validCount2)
