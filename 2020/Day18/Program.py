1823b754b8957438240d356f3cb1afaea978a54e
danksalot <danksalot@gmail.com>
2020-12-17 23:44:36 -0600

    2020 Day18

--- 2020/Day18/Program.py
+++ 2020/Day18/Program.py
@@ -1,5 +1,31 @@
-with open('Input') as inFile:
-	lines = [int(x) for x in inFile.readlines()]
+import re
 
-	for line in lines:
-		print(line)
+class Hack:
+	def __init__(self, value):
+		self.value = value
+	def __add__(self, other):
+		return Hack(self.value + other.value)
+	def __sub__(self, other):
+		return Hack(self.value * other.value)
+	def __mul__(self, other):
+		return Hack(self.value + other.value)
+
+with open('Input') as inFile:
+	lines = inFile.read().splitlines()
+
+total = 0
+for line in lines:
+	line = re.sub(r'(\d)', r'Hack(\1)', line)
+	line = line.replace('*', '-')
+	total += eval(line, {'Hack': Hack}).value
+
+print('Part 1:', total)
+
+total = 0
+for line in lines:
+	line = re.sub(r'(\d)', r'Hack(\1)', line)
+	line = line.replace('*', '-')
+	line = line.replace('+', '*')
+	total += eval(line, {'Hack': Hack}).value
+
+print('Part 2:', total)
