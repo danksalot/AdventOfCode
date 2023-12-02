eed737fe7318fed6adaa0989f1e7ac8735710037
danksalot <danksalot@gmail.com>
2023-12-01 22:40:19 -0700

    Tidying

--- 2023/Day02/Program.py
+++ 2023/Day02/Program.py
@@ -3,42 +3,17 @@ import re
 with open('Input') as inFile:
 	lines = inFile.readlines()
 
-maxRed = 12
-maxGreen = 13
-maxBlue = 14
 total = 0
 totalPower = 0
 
 for line in lines:
-	minRed = 0
-	minGreen = 0
-	minBlue = 0
-	possible = True
+	maxReds = max([int(r) for r in re.findall(r'(\d+) red', line)])
+	maxGreens = max([int(g) for g in re.findall(r'(\d+) green', line)])
+	maxBlues = max([int(b) for b in re.findall(r'(\d+) blue', line)])
 
-	digits = re.findall(r'\d+', line)
-	id = int(digits[0])
-
-	reds = [int(r) for r in re.findall(r'(\d+) red', line)]
-	if max(reds) > minRed:
-		minRed = max(reds)
-	if max(reds) > maxRed:
-		possible = False
-
-	greens = [int(g) for g in re.findall(r'(\d+) green', line)]
-	if max(greens) > minGreen:
-		minGreen = max(greens)
-	if max(greens) > maxGreen:
-		possible = False
-
-	blues = [int(b) for b in re.findall(r'(\d+) blue', line)]
-	if max(blues) > minBlue:
-		minBlue = max(blues)
-	if max(blues) > maxBlue:
-		possible = False
-
-	if possible:
-		total += id
-	totalPower += minRed * minGreen * minBlue
+	if maxReds <= 12 and maxBlues <= 14 and maxGreens <= 13: 
+		total += int(re.findall(r'(\d+)', line)[0])	
+	totalPower += maxReds * maxGreens * maxBlues
 	
 print('Part 1: ', total)
 print('Part 2: ', totalPower)
