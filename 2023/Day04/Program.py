1240de0a6f47e9a1ee23c2989f40198c7e2951e6
danksalot <danksalot@gmail.com>
2023-12-03 22:34:22 -0700

    Day04

--- 2023/Day04/Program.py
+++ 2023/Day04/Program.py
@@ -1,4 +1,30 @@
-with open('Input') as inFile:
-	lines = inFile.read()
+import re
+import collections
 
-print(lines)
+with open('Input') as inFile:
+	lines = inFile.readlines()
+
+def getNumWinners(winString, haveString):
+	winners = re.findall(r'\d+', winString)
+	have = re.findall(r'\d+', haveString)
+	result = collections.Counter(winners) & collections.Counter(have)
+	return len(list(result.elements()))
+
+total = 0
+for line in lines:
+	parts = line.split(':')[1].split('|')
+	numWinners = getNumWinners(parts[0], parts[1])
+
+	if numWinners > 0:
+		total += 2 ** (numWinners - 1)
+
+print('Part 1:', total)
+
+numCards = [1] * len(lines)
+for i, line in enumerate(lines):
+	parts = line.split(':')[1].split('|')
+	numWinners = getNumWinners(parts[0], parts[1])
+	for x in range(1, numWinners + 1):
+		numCards[i + x] += numCards[i]
+
+print('Part 2:', sum(numCards))
