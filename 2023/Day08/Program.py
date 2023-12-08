a30a1a5b27c409f1dfbee32a4388e2ad8032cd36
danksalot <danksalot@gmail.com>
2023-12-07 23:04:13 -0700

    Day08

--- 2023/Day08/Program.py
+++ 2023/Day08/Program.py
@@ -1,4 +1,51 @@
-with open('Input') as inFile:
-	lines = inFile.read()
+from itertools import cycle
+from functools import reduce
+import numpy as np
 
-print(lines)
+with open('Input') as inFile:
+	lines = inFile.readlines()
+
+def extractNode(nodeString):
+	name = nodeString[0:3]
+	left = nodeString[7:10]
+	right = nodeString[12:15]
+	return name, [left, right]
+	
+
+instructions = cycle(lines[0].strip())
+nodes = {}
+for node in lines[2:]:
+	name, paths = extractNode(node)
+	nodes[name] = paths
+
+currentNode = 'AAA'
+steps = 0
+for instruction in instructions:
+	steps += 1
+	if instruction == 'L':
+		currentNode = nodes[currentNode][0]
+	elif instruction == 'R':
+		currentNode = nodes[currentNode][1]
+	if currentNode == 'ZZZ':
+		break
+
+print('Part 1:', steps)
+
+currentNodes = [node for node in nodes if node[2] == 'A']
+stepSizes = []
+for node in currentNodes:
+	cn = node
+	finishingStates = []
+	steps = 0
+	for instruction in instructions:
+		steps += 1
+		if instruction == 'L':
+			node = nodes[node][0]
+		elif instruction == 'R':
+			node = nodes[node][1]
+		if node[2] == 'Z':
+			stepSizes.append(steps)
+			break
+
+leastCommonMultiple = reduce(lambda x, y: np.lcm(x,y,dtype=object), stepSizes)
+print('Part 2:', leastCommonMultiple)
