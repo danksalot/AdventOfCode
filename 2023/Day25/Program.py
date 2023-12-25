193aea91c691cb590e7e75d8375c8a9c1c272723
danksalot <danksalot@gmail.com>
2023-12-24 23:40:00 -0700

    Day25

--- 2023/Day25/Program.py
+++ 2023/Day25/Program.py
@@ -1,4 +1,20 @@
-with open('Input') as inFile:
-	lines = inFile.read()
+import networkx as nx
 
-print(lines)
+with open('Input') as inFile:
+	lines = [x.strip() for x in inFile.readlines()]
+
+G = nx.Graph()
+
+for line in lines:
+	parts = line.split(': ')
+	rightparts = parts[1].split(' ')
+	for part in rightparts:
+		G.add_edge(parts[0], part)
+
+G.remove_edges_from(nx.minimum_edge_cut(G))
+
+product = 1
+for group in nx.connected_components(G):
+	product *= len(group)
+
+print('Part 1:', product)
