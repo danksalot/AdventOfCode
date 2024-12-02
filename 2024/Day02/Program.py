904128924cce900f97912420439a0a0dfbfb5546
danksalot <danksalot@gmail.com>
2024-12-01 22:39:45 -0700

    2024 Day02

--- 2024/Day02/Program.py
+++ 2024/Day02/Program.py
@@ -1,13 +1,25 @@
 
+def isSafe(levels):
+	diffs = [int(levels[i]) - int(levels[i + 1]) for i in range(len(levels) - 1)]
+	allAscending = all(diff > 0 for diff in diffs)
+	allDescending = all(diff < 0 for diff in diffs)
+	allWithinRange = all(abs(diff) > 0 and abs(diff) < 4 for diff in diffs)
+	return (allAscending or allDescending) and allWithinRange
+
 with open('Input') as inFile:
-	lines = inFile.readlines()
+	reports = [line.rstrip() for line in inFile.readlines()]
 
-for line in lines:
-	print(line)
+print("Part 1:", sum(1 for report in reports if isSafe(report.split(' '))))
 
-print("Part 1:", len(lines))
+safeCount = 0
+for report in reports:
+	levels = report.split(' ')
+	if (isSafe(levels)):
+		safeCount += 1
+	else:
+		for i in range(len(levels)):
+			if (isSafe(levels[0:i] + levels[i+1:])):
+				safeCount += 1
+				break
 
-for line in lines:
-	print(line)
-
-print("Part 2:", len(lines))
+print("Part 2:", safeCount)
