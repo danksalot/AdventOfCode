315365e0b6659870b218051c05196a0a668d0d3e
danksalot <danksalot@gmail.com>
2024-12-04 23:16:33 -0700

    2024 Day05

--- 2024/Day05/Program.py
+++ 2024/Day05/Program.py
@@ -1,13 +1,24 @@
 
 with open('Input') as inFile:
-	lines = inFile.readlines()
+	lines = [line.rstrip('\n') for line in inFile.readlines()]
 
-for line in lines:
-	print(line)
+rules = [list(map(int, line.split('|'))) for line in lines if '|' in line]
+updates = [list(map(int, line.split(','))) for line in lines if ',' in line]
 
-print("Part 1:", len(lines))
+def findBrokenRule(update):
+	for i in range(len(update)):
+		brokenRules = [r for r in rules if r[1] == update[i] and r[0] in update[i:]]
+		if brokenRules:
+			return brokenRules[0]
+	return None
 
-for line in lines:
-	print(line)
+def fixUpdate(update):
+	ruleBroken = findBrokenRule(update)
+	while ruleBroken:
+		a, b = update.index(ruleBroken[0]), update.index(ruleBroken[1])
+		update[b], update[a] = update[a], update[b]
+		ruleBroken = findBrokenRule(update)
+	return update
 
-print("Part 2:", len(lines))
+print("Part 1:", sum(update[len(update)//2] for update in updates if findBrokenRule(update) is None))
+print("Part 2:", sum(fixUpdate(update)[len(update)//2] for update in updates if findBrokenRule(update)))
